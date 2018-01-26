dc897c050aadde2696bbbe0e1c7e7265888ff3fc
Unknown <rafael@rleite.com.br>
2018-01-26 15:48:40 -0300

    Controller Categoria

--- api/src/categoria/categoria.controller.js
+++ api/src/categoria/categoria.controller.js
@@ -1,4 +1,6 @@
 const Categoria = require('./categoria.model')
+const Projeto = require('../projeto/projeto.model')
+const mongoose = require('mongoose')
 const querystring = require('query-string')
 const ERRORS = require('../constants').ERRORS
 
@@ -9,7 +11,7 @@ const ERRORS = require('../constants').ERRORS
 exports.getCategorias = async (ctx, next) => {
     try {
         const categorias = await Categoria.find({})
-        const categoriasDTO = categorias.map(cat => ({ id: cat._id, nome: cat.nome }))
+        const categoriasDTO = categorias.map(cat => montaCategoriaDTO(cat))
         ctx.status = 200
         ctx.body = { categorias: categoriasDTO }
         await next()
@@ -24,7 +26,13 @@ exports.getCategorias = async (ctx, next) => {
  */
 exports.getCategoria = async (ctx, next) => {
     try {
-
+        const _id = new mongoose.mongo.ObjectID(ctx.params.id)
+        const categoria = await Categoria.findById(_id)
+        const categoriaDTO = montaCategoriaDTO(categoria)
+        
+        ctx.body = categoriaDTO
+        ctx.status = 200
+        await next()
     } catch (err) {
         ctx.throw(500, err)
     }
@@ -33,34 +41,68 @@ exports.getCategoria = async (ctx, next) => {
 exports.newCategoria = async (ctx, next) => {
     try {
         let categoria = await Categoria.findOne({ nome: ctx.request.body.nome })
-        console.log(categoria)
         if (categoria !== null) {
-            console.log('passou aqui')
             ctx.status = 422
             ctx.body = { error: ERRORS.CATEGORIA_EXISTE }
             return await next()
         }
-        console.log('passou aqui de fora')
         categoria = new Categoria({ nome: ctx.request.body.nome })
-        ctx.body = await categoria.save()
+        categoria = await categoria.save() 
+        ctx.body = montaCategoriaDTO(categoria)
         await next()
     } catch (err) {
         ctx.throw(500, err)
     }
 }
 
+/**
+ * editCategoria  - Edita a categoria
+ */
 exports.editCategoria = async (ctx, next) => {
     try {
+        const _id = new mongoose.mongo.ObjectID(ctx.params.id)
+        const categoriaExist = await Categoria.findOne({ nome: ctx.request.body.nome, _id: {$ne: _id} })
+        if(categoriaExist !== null) {
+            ctx.status = 422
+            ctx.body = { error: ERRORS.CATEGORIA_EXISTE }
+            return await next()
+        }
 
+        await Categoria.findOneAndUpdate({ _id: ctx.params.id }, { nome: ctx.request.body.nome });
+        ctx.status = 200
+        ctx.body = { msg: 'Categoria alterada com sucesso', status: 200 }
+        await next();
     } catch (err) {
         ctx.throw(500, err)
     }
 }
 
+/**
+ * deleteCategoria  - Apaga uma categoria
+ */
 exports.deleteCategoria = async (ctx, next) => {
     try {
+        const _id = new mongoose.mongo.ObjectID(ctx.params.id)
+        const projetos = await Projeto.find({ categoria : _id })
+        
+        if(projetos !== null && projetos.length > 0) {
+            ctx.status = 400
+            ctx.body = { error: 'Existem projetos vinculados a categoria', status: 400 }
+            return await next()
+        }
 
+        await Categoria.findOneAndRemove({ _id: ctx.params.id });
+        ctx.status = 200
+        ctx.body = { msg: 'Categoria apagada com sucesso!', status: 200 }
+        await next()
     } catch (err) {
         ctx.throw(500, err)
     }
+}
+
+montaCategoriaDTO = (categoria) => {
+    if(categoria === null || categoria === undefined) {
+        return {}
+    }
+    return { id: categoria._id, nome: categoria.nome }
 }
--- api/src/categoria/categoria.route.js
+++ api/src/categoria/categoria.route.js
@@ -6,7 +6,7 @@ const router = new Router({ prefix: '/categorias' })
 router.get('/', controller.getCategorias)
 router.get('/:id', controller.getCategoria)
 router.post('/', controller.newCategoria)
-router.put('/', controller.editCategoria)
-router.delete('/', controller.deleteCategoria)
+router.put('/:id', controller.editCategoria)
+router.delete('/:id', controller.deleteCategoria)
 
 module.exports = router
